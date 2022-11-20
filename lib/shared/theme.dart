0966dfa71dc051c3f066f5490982d2db77915b5a
rizky ferdian <rizkyferdian002@gmail.com>
2022-11-20 21:26:57 +0700

    First Commit & add Theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color whiteColor = const Color(0xffFFFFFF);
+Color blackColor = const Color(0xff14193F);
+Color greyColor = const Color(0xffA4A8AE);
+Color lightGreyColor = const Color(0xffF1F1F9);
+Color lightBackgroundColor = const Color(0xffF6F8FB);
+Color darkBackgroundColor = const Color(0xff020518);
+Color blueColor = const Color(0xff53C1F9);
+Color purpleColor = const Color(0xff5142E6);
+Color greenColor = const Color(0xff22B07D);
+Color numberBackgroundColor = const Color(0xff1A1D2E);
+Color redColor = const Color(0xffFF2566);
