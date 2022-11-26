2aea8655c28e9424faf6ef9d0c73042d55e49b2a
rizky ferdian <rizkyferdian002@gmail.com>
2022-11-26 21:57:09 +0700

    add onboarding

--- lib/ui/pages/SplashPage.dart
+++ lib/ui/pages/SplashPage.dart
@@ -1,8 +1,31 @@
+import 'dart:async';
+
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/pages/onboarding_page.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnboardingPage(),
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
